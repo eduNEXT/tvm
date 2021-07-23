ced1ff6bb3838f7f6bbc9fe81f56cbdb1eee9c6c
María Fernanda Magallanes Z <maria.magallanes@edunext.co>
2021-07-23 11:11:37 -0400

    fix: remove the unnecessary arg in setup_tvm and improve a description in the readme

--- stack/tvm.py
+++ stack/tvm.py
@@ -52,7 +52,7 @@ def validate_version_installed(ctx, param, value):  # pylint: disable=unused-arg
     return value
 
 
-def setup_tvm(clear: bool = False):
+def setup_tvm():
     """
     Initialize the directory for all tutor versions.
 
@@ -64,7 +64,7 @@ def setup_tvm(clear: bool = False):
         pass
 
     info_file_path = f'{TVM_PATH}/current_bin.json'
-    if not os.path.exists(info_file_path) or clear:
+    if not os.path.exists(info_file_path):
         data = {
             "active": None,
             "tutor_root": None,
