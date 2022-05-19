60658fd34bfeb8e407b5675c7555fdb32b221afa
Alejandro Cardenas <alecar.main@gmail.com>
2022-05-19 08:29:40 -0500

    feat: add tvm config commands

--- tvm/templates/tutor_switcher.py
+++ tvm/templates/tutor_switcher.py
@@ -3,16 +3,13 @@ from jinja2 import Template
 
 TEMPLATE = '''
 {% if tutor_root %}
-echo -e "Using the \033[1;32m{{ config_name }}\033[0m config" >&2
 export TUTOR_ROOT={{ tutor_root }}
-export TUTOR_PLUGINS_ROOT={{ tutor_root }}/inline_plugins
-{% else %}
-echo -e "You have not selected any config. Tutor will use the global default \033[1;31m~.local/share/tutor\033[0m"
+export TUTOR_PLUGINS_ROOT={{ tutor_plugins_root }}
 {% endif %}
 {% if version %}
 {{ tvm }}/{{ version }}/venv/bin/tutor $@
 {% else %}
-echo "You need to select a tutor active version at first. Run 'stack tvm use <VERSION>'"
+echo "You need to select a tutor active version at first. Run 'tvm use <VERSION>'"
 {% endif %}
 '''
 
